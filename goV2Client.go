1eccfa27f1456cea1db80de418222fe373b71d67
Simoncao <13671707129@126.com>
2023-01-29 17:55:28 +0800

    [feat]sub add func

--- goV2Client.go
+++ goV2Client.go
@@ -1,36 +1,10 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"goV2Client/sub"
 	"os"
 )
-
-var (
-	subUrl = flag.String("sub", "", "订阅地址默认为空")
-)
-
-/**
-func main() {
-	//解析命令行参数
-	flag.Parse()
-
-	//通过订阅url获取base64加密后的返回
-	res := curl.Cget(*subUrl)
-
-	//解密base64  获取节点
-	nodes := b64Decoder.Decoder([]byte(res))
-
-	//逐行解析出节点
-	scanner := bufio.NewScanner(strings.NewReader(string(nodes)))
-	for scanner.Scan() {
-		decodeStr := strings.Split(scanner.Text(), "//")[1]
-		node := b64Decoder.Decoder([]byte(decodeStr))
-		fmt.Println(string(node))
-	}
-}
-**/
 
 func main() {
 	args := os.Args

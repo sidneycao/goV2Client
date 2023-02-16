403f83db11d05a66296f27628e93a837e767e280
Simoncao <13671707129@126.com>
2023-02-16 17:20:24 +0800

    [feat]cloes speedtest output

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/SidneyCao/goV2Client
 
 go 1.18
 
-require github.com/SidneyCao/tcping v0.0.6
+require github.com/SidneyCao/tcping v0.0.7
--- tools/speedtest/speedtest.go
+++ tools/speedtest/speedtest.go
@@ -1,6 +1,7 @@
 package speedtest
 
 import (
+	"bytes"
 	"time"
 
 	"github.com/SidneyCao/tcping/utils"
@@ -8,7 +9,9 @@ import (
 
 func Start(host string, port string, timeout string, interval string, counters int) time.Duration {
 	t := utils.NewTarget(host, port, timeout, interval)
-	pinger := utils.NewPing(*t, counters)
+	// 不输出到os.Stdout
+	var tmp_buff bytes.Buffer
+	pinger := utils.NewPing(*t, &tmp_buff, counters)
 	go pinger.Ping()
 	// 等待ping完成
 	<-pinger.Done()
